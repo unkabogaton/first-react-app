569a1c80f84884bea02c4618741ee60c7c0f1031
unkabogaton <anthonralph2@gmail.com>
2023-08-16 16:54:40 +0800

    installing tailwind

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,8 @@
 function Message() {
-    return <h1>Hello World</h1>
+    const myName = 'Mosh';
+    if (myName)
+        return <h1>Hello {myName}</h1>;
+    return <h1>Hello World</h1>;
 }
 
 export default Message;
